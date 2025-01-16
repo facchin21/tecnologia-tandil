a8104631aa2a0deff53986e0ad22e58c30050068
facchin21 <ferminfacchinq@gmail.com>
2025-01-16 10:53:41 -0300

    agrego contenido de productos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -21,10 +21,15 @@ export default {
         "0%": { transform: "translateY(-100%)", opacity: "0" },
         "100%": { transform: "translateY(0)", opacity: "1" },
       },
+      slideRight: {
+        "0%": { transform: "translateX(100%)", opacity: "0" },
+        "100%": { transform: "translateX(0)", opacity: "1" },
+      },
     },
     animation: {
       slideIn: "slideIn 1s ease-out",
       slideTop: "slideTop 1s ease-out",
+      slideRight: "slideRight 1s ease-out",
     },
   },
   plugins: [],
